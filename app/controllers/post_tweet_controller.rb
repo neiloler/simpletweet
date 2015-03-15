aa6e4bc275252c92d530f3d1202e67bebe537ee2
Neil Oler <neil@nuvi.com>
2015-03-14 18:57:39 -0600

    Writing out simple TODO list for the controller.

--- app/controllers/post_tweet_controller.rb
+++ app/controllers/post_tweet_controller.rb
@@ -3,8 +3,16 @@ class PostTweetController < ApplicationController
   end
 
   def submit
+
+    # Scratch work
     puts "PARAMS!"
     puts params[:tweet_contents]
+
+    # TODO Authenticate as needed
+
+    # TODO Make simple API POST call to Twitter API
+    
+    # Get us back to a fresh post screen
     redirect_to :action => "index"
   end
 end
